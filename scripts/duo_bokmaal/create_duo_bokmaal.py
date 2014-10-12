3a77019864545964584dc56d309290550113fd09
sourcebrowse <sourcebrowse@gmail.com>
2014-10-12 17:46:07 +0200

    Finished creating the DUO corpus, organised in English, Bokmaal, Nynorsk, and Corrupt files.

--- scripts/duo_bokmaal/create_duo_bokmaal.py
+++ scripts/duo_bokmaal/create_duo_bokmaal.py
@@ -1,7 +1,7 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-import time
+import shutil
 
 
 class Detection():
@@ -24,12 +24,20 @@ class Detection():
         # 'by', 'she', 'or', 'as', 'what'
         self.en = ['the', 'be', 'and', 'of', 'in', 'to', 'have', 'it']
 
+        self.word_count_en_bm_nn = {'en': 0,
+                      'bm': 0,
+                      'nn': 0}
+
+        self.en_count = 0
         self.bm_count = 0
         self.nn_count = 0
-        self.en_count = 0
+        self.corrupt_count = 0
+        self.all_count = 0
 
-    def read_files(self, path='/Users/arashsaidi/Work/TextLab/Code/academic_dictionary/corpus/duo/1-10/'):
-        """ Recursively traverses all files
+        self.info = open('info.txt', 'w')
+
+    def read_files(self, path='/Users/arashsaidi/Work/TextLab/Code/academic_dictionary/corpus/duo/'):
+        """ Recursively traverses all files in the path
 
         :param path: path to corpus
         :return: None
@@ -40,37 +48,70 @@ class Detection():
                 if '.txt' in f:
                     current_path_to_file = os.path.join(root, f)
                     current_file = open(current_path_to_file, 'r')
-                    self.check_language(current_file)
+                    self.check_language(current_file, current_path_to_file)
 
-    def check_language(self, text_file):
+    def check_language(self, text_file, path_to_file):
         """ Checks words in file for occurrences in lists
         :param text_file: The current text_file
         :return: None
         """
-        test_count = 0
-        first_lines = ''
         for line in text_file.readlines():
-            if test_count < 10:
-                first_lines += line
-                test_count += 1
             for word in line.split():
                 lower_case = word.lower()
                 if lower_case in self.bm:
-                    self.bm_count += 1
+                    self.word_count_en_bm_nn['bm'] += 1
                 elif lower_case in self.nn:
-                    self.nn_count += 1
+                    self.word_count_en_bm_nn['nn'] += 1
                 elif lower_case in self.en:
-                    self.en_count += 1
+                    self.word_count_en_bm_nn['en'] += 1
 
-        self.move_file(text_file, first_lines)
+        self.move_file(path_to_file)
 
-    def move_file(self, text_file, first_lines=''):
-        print "For file: {}".format(first_lines)
-        print "bm count: {} \nnn count: {} \nen count: {}".format(self.bm_count, self.nn_count, self.en_count)
-        self.bm_count = 0
-        self.nn_count = 0
-        self.en_count = 0
-        time.sleep(7)
+    def move_file(self, path_to_file):
+
+        key = max(self.word_count_en_bm_nn, key=self.word_count_en_bm_nn.get)
+        # print "largest {}\n".format(key)
+
+        if self.word_count_en_bm_nn[key] < 50:
+            # print "File may be corrupted"
+            # self.write_info(current_file)
+            self.corrupt_count += 1
+            filename = 'DUO_Corrupt_' + str(self.corrupt_count) + '.txt'
+            shutil.copyfile(path_to_file, "../../corpus/DUO_Corpus/Corrupted/" + filename)
+        elif key == 'bm':
+            self.bm_count += 1
+            filename = 'DUO_BM_' + str(self.bm_count) + '.txt'
+            shutil.copyfile(path_to_file, "../../corpus/DUO_Corpus/Bokmaal/" + filename)
+        elif key == 'nn':
+            self.nn_count += 1
+            filename = 'DUO_NN_' + str(self.nn_count) + '.txt'
+            shutil.copyfile(path_to_file, "../../corpus/DUO_Corpus/Nynorsk/" + filename)
+        elif key == 'en':
+            self.en_count += 1
+            filename = 'DUO_English_' + str(self.en_count) + '.txt'
+            shutil.copyfile(path_to_file, "../../corpus/DUO_Corpus/English/" + filename)
+
+        # if self.all_count % 100 == 0:
+        #     self.write_info(current_file)
+
+        self.word_count_en_bm_nn['bm'] = 0
+        self.word_count_en_bm_nn['nn'] = 0
+        self.word_count_en_bm_nn['en'] = 0
+        self.all_count += 1
+
+    def write_info(self):
+        self.info.write("-" * 30)
+        self.info.write("DUO corpus:")
+        self.info.write("English articles: {}\nBM articles: {}\nNN articles: {}\nCorrupt articles: {}".format(
+            test.en_count,
+            test.bm_count,
+            test.nn_count,
+            test.corrupt_count))
+        self.info.write("-" * 30)
 
 test = Detection()
 test.read_files()
+print "English articles: {}\nBM articles: {}\nNN articles: {}\nCorrupt articles: {}".format(test.en_count,
+                                                                                            test.bm_count,
+                                                                                            test.nn_count,
+                                                                                            test.corrupt_count)
