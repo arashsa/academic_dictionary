0d189b9c5d91c766069ebab823e444ec7bcc6d94
sourcebrowse <sourcebrowse@gmail.com>
2014-10-16 15:45:48 +0200

    Changed import structure in main

--- scripts/academic_wordlist_generator/main.py
+++ scripts/academic_wordlist_generator/main.py
@@ -1,15 +1,15 @@
 __author__ = 'arashsaidi'
 
-from scripts.academic_wordlist_generator.create_word_lists import *
-from scripts.academic_wordlist_generator.printing import *
-from scripts.academic_wordlist_generator.read_corpus import *
-from scripts.academic_wordlist_generator.write_to_file import *
+from create_word_lists import *
+from printing import *
+from read_corpus import *
+from write_to_file import *
 import time
 
 
 def create_list(print_alpha=False, print_val=False, cutoff=15):
-    create_most_freq_word_list('/Users/arashsaidi/Work/TextLab/Code/academic_dictionary/1000_hifreq_lemmas_forms.txt')
-    create_english_word_list('/Users/arashsaidi/Work/TextLab/Code/academic_dictionary/brit-a-z.txt')
+    create_most_freq_word_list('../1000_hifreq_lemmas_forms.txt')
+    create_english_word_list('../brit-a-z.txt')
     reduced_frequency(cutoff)
     remove_most_frequent_words_numbers_english()
     remove_relative_frequent_words_below_score(cutoff)
@@ -21,7 +21,8 @@ def create_list(print_alpha=False, print_val=False, cutoff=15):
 
 
 start_time = time.time()
-read_all_files('/Users/arashsaidi/Work/TextLab/Code/academic_dictionary/corpus/DUO_Corpus/Bokmaal/*.txt', 'txt')
+print 'Starting Script: Create Academic Word List'
+read_all_files('../corpus/DUO_Corpus/Bokmaal/*.txt', 'txt')
 create_list()
 write_dict_to_file(get_global_reduced_freqs())
 print("--- %s seconds ---" % (time.time() - start_time))
